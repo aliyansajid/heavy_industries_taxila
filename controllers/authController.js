3b75d847f5caf77afb782bfe791c074425d46b04
Aliyan Sajid <aliyansajid127@gmail.com>
2024-10-05 16:38:14 +0500

    Develop and implement a secure user authentication system

---
+++ controllers/authController.js
@@ -0,0 +1,36 @@
+const { PrismaClient } = require("@prisma/client");
+const bcrypt = require("bcrypt");
+
+const prisma = new PrismaClient();
+
+exports.loginUser = async (req, res) => {
+  const { username, password, role } = req.body;
+  try {
+    const user = await prisma.user.findUnique({
+      where: { username },
+    });
+
+    if (!user) {
+      return res.status(404).json({ error: "Invalid username" });
+    }
+
+    const validPassword = await bcrypt.compare(password, user.password);
+    if (!validPassword) {
+      return res.status(401).json({ error: "Invalid password" });
+    }
+
+    if (user.role !== role) {
+      return res.status(403).json({ error: "Invalid role" });
+    }
+
+    res.status(200).json({
+      id: user.id,
+      name: user.username,
+      email: user.email,
+      role: user.role,
+    });
+  } catch (error) {
+    console.error("Login failed:", error);
+    res.status(500).json({ error: "Login failed" });
+  }
+};
--- server.js
+++ server.js
@@ -1,6 +1,8 @@
 const express = require("express");
 const bodyParser = require("body-parser");
 const cors = require("cors");
+
+const authRoutes = require("./routes/authRoutes");
 const userRoutes = require("./routes/userRoutes");
 const departmentRoutes = require("./routes/departmentRoutes");
 
@@ -9,6 +11,7 @@ const app = express();
 app.use(cors());
 app.use(bodyParser.json());
 
+app.use("/api/login", authRoutes);
 app.use("/api/users", userRoutes);
 app.use("/api/departments", departmentRoutes);
 
